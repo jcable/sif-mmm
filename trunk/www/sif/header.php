9e7181d90d7f6b69ffdd4122ad8600eecbc60dcf
jfbcable <jfbcable@372de4ae-fb28-11dd-ac5b-dfceb64ead24>
2009-09-25 14:44:21 +0000

    starting to factor out the header.

---
+++ trunk/www/sif/header.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>SIF Project <?php print $titleword; ?></title>
+</head>
+<link rel="stylesheet" type="text/css" href="main.css" media="screen,print">
+<body>
+<table border=0 width=100%><tr><th width=80px><img src="wslogo.jpg" alt=""></th>
+<th class="menubutton" onclick="location.href='index.html';">Main Menu</th>
+<th class="menubutton" onclick="location.href='showserviceschedule.php';">Service Schedules</th>
+<th class="menubutton" onclick="location.href='showlistenerschedule.php';">Listener Schedules</th>
+<th class="menubutton" onclick="location.href='showmaterial.php';">Material Info</th>
+<th class="menubutton" onclick="location.href='servicecrashswitch.php?sourcetab=1&servicetab=1';">Crash Services</th>
+<th class="menubutton"onclick="location.href='listenercrashswitch.php?servicetab=1&listenertab=1';">Crash Listeners</th>
+<th class="menubutton" onclick="location.href='monitor.php';">Monitoring</th>
+<th class="menubutton" onclick="location.href='sourcepairs.php';">Redundant Sources</th>
+</tr><tr>
+<td></table>
+<h3>
+SIF Project - <?php print $titleword; ?></h3>
