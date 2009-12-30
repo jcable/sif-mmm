eec2d170bb63c1e1987d0bc5979531116ba731e5
jfbcable <jfbcable@372de4ae-fb28-11dd-ac5b-dfceb64ead24>
2009-12-30 09:52:31 +0000

    use libtwolame to stream. Code gleaned from OpenCaster and others. Work In Progress

---
+++ trunk/client/mp2stream/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+	take audio and encode it as MP2 and stream it
+
+	parameters
+
+	alsa audio port
+	mono/stereo
+	bitrate
+	serviceid
+	audio pid
+	pmt pid
+	initial PTS
+	initial PCE
+	start time absolute to ms accuracy
+	number of 1152 audio samples mpeg frames to encode
+
+*/
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
