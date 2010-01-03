076412e3a7cbc34ba9289b7c49bf6a6215ff2521
jfbcable <jfbcable@372de4ae-fb28-11dd-ac5b-dfceb64ead24>
2010-01-03 22:44:13 +0000

    RabbitMQ Example

---
+++ client/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace SimplePublishSubscribe
+{
+    class Program
+    {
+        private static void PrintMessage(object sender, byte[] message)
+        {
+            Console.WriteLine(System.Text.Encoding.UTF8.GetString(message));
+        }
+
+        static void Main(string[] args)
+        {
+            string hostName = "dev.rabbitmq.com";
+            hostName = "192.168.2.137";
+            int portNumber = 5672;
+            string exchangeName = "sif";
+            string routingKey = "studio1";
+            if (args.Length > 0)
+            {
+                Console.WriteLine("listening");
+                Listener listener = new Listener(exchangeName, hostName, portNumber, routingKey, args[0]);
+                listener.MessageReceived += new MessageHandler(PrintMessage);
+                listener.listen();
+            }
+            else
+            {
+                Console.WriteLine("sending");
+                Sender sender = new Sender(exchangeName, hostName, portNumber, routingKey);
+                while (true)
+                {
+                    byte[] messageBodyBytes = System.Text.Encoding.UTF8.GetBytes(Console.ReadLine());
+                    sender.send(messageBodyBytes);
+                }
+            }
+        }
+    }
+}
---
+++ client/SimplePublishSubscribe.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace SimplePublishSubscribe
+{
+    public delegate void MessageHandler(object sender, byte[] message);
+
+    class Client
+    {
+        protected string exchangeName;
+        protected string routingKey;
+        protected IConnection conn;
+        protected IModel channel;
+
+        public Client(string eName, string key, string hostName, int portNumber)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            IProtocol protocol = Protocols.FromEnvironment();
+            conn = factory.CreateConnection(protocol, hostName, portNumber);
+            channel = conn.CreateModel();
+            exchangeName = eName;
+            channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+            routingKey = key;
+        }
+    }
+
+    class Sender : Client
+    {
+        public Sender(string exchangeName, string hostName, int portNumber, string key)
+            :             base(exchangeName, key, hostName, portNumber)
+        {
+        }
+
+        public void send(byte[] message)
+        {
+            channel.BasicPublish(exchangeName, routingKey, null, message);
+        }
+    }
+
+    class Listener : Client
+    {
+        public event MessageHandler MessageReceived;
+        QueueingBasicConsumer consumer;
+        
+        public Listener(string exchangeName, string hostName, int portNumber, string key, string queueName)
+            : base(exchangeName, key, hostName, portNumber)
+        {
+            channel.QueueDeclare(queueName);
+            channel.QueueBind(queueName, exchangeName, routingKey, false, null);
+            consumer = new QueueingBasicConsumer(channel);
+            channel.BasicConsume(queueName, null, consumer);
+        }
+
+        public void listen()
+        {
+            while (true)
+            {
+                try
+                {
+                    RabbitMQ.Client.Events.BasicDeliverEventArgs e =
+                    (RabbitMQ.Client.Events.BasicDeliverEventArgs)
+                    consumer.Queue.Dequeue();
+                    IBasicProperties props = e.BasicProperties;
+                    // ... process the message
+                    MessageReceived(this, e.Body);
+                    channel.BasicAck(e.DeliveryTag, false);
+                }
+                catch (RabbitMQ.Client.Exceptions.OperationInterruptedException ex)
+                {
+                    // The consumer was removed, either through
+                    // channel or connection closure, or through the
+                    // action of IModel.BasicCancel().
+                    break;
+                }
+            }
+        }
+    }
+}
