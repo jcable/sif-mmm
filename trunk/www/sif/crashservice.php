7882b1b7d03c1bc0ae4ef1f3d9573c353e58e9c2
jfbcable <jfbcable@372de4ae-fb28-11dd-ac5b-dfceb64ead24>
2010-01-06 23:33:35 +0000

    ithoughts on crashing

--- trunk/www/sif/crashservice.php
+++ trunk/www/sif/crashservice.php
@@ -6,10 +6,21 @@ $dbh = new PDO(
 if (isset($_REQUEST["service"]))
 {
 	header("location: servicecrashswitch.php?sourcetab=".$_REQUEST["sourcetab"]."&servicetab=".$_REQUEST["servicetab"]);
-	//mysql_query("update service set current_source='$source' where service='$service'", $connection);
+
+/*
+   make a new schedule entry - but when should it end ?
+
+how about - if "hold" is true then it ends at midnight unless we are after 23:00 in which case it ends at the next midnight
+
+if hold is false then it ends at the next event
+
+OR - we have events, not schedule periods and we don't need an end!
+
+ALSO we should have a priority field in the schedule which would allow it to work like a stack.
+
 	$stmt = $dbh->prepare("INSERT INTO service_active_schedule (first_date,start_time,service,source) VALUES(?,?,?,?)");
-	$stmt->bindParam(1, $today);
-	$stmt->bindParam(2, $now);
+	$stmt->bindParam(1, strftime("%Y-%m-%d"));
+	$stmt->bindParam(2, strftime("%T");
 	$stmt->bindParam(3, $_REQUEST["service"]);
 	$stmt->bindParam(4, $_REQUEST["source"]);
 	$stmt->execute();
