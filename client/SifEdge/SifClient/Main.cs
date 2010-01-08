7dfc039236fb5c147c7fdb69a1dba9ae039e356f
jfbcable <jfbcable@372de4ae-fb28-11dd-ac5b-dfceb64ead24>
2010-01-08 20:14:04 +0000

    added SifClient to SifEdge.sln. converted arrays to lists moved vlc invoke to SifClient

---
+++ client/SifEdge/SifClient/Main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SifClient
+{
+    class MainClass
+    {
+        public static int Main(string [] args)
+        {
+            SifClient client = new SifClient();
+            return 0;
+        }
+    }
+}
