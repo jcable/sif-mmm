b039f7cc0120b9c40c7e181d64d597ea46ddbb28
jonnyrobertshaw <jonnyrobertshaw@372de4ae-fb28-11dd-ac5b-dfceb64ead24>
2010-01-08 16:35:53 +0000

    test

--- client/SifEdge/SifSource/Main.cs
+++ client/SifEdge/SifSource/Main.cs
@@ -2,15 +2,16 @@ using System;
 
 namespace SifSource
 {
-	class MainClass
-	{
-		public static void Main(string[] args)
-		{
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
             Source source = new Source(
                 args[0], args[1], args[2], args[3],
-                args[4]=="true");
-                //"http://ws13.dyndns.ws/sif", "Player 1", "sif-03", "analog2", false);
-			Console.WriteLine(source.ToString());
-		}
-	}
+                args[4] == "true");
+            //"http://ws13.dyndns.ws/sif", "Player 1", "sif-03", "analog2", false);
+            Console.WriteLine(source.ToString());
+        }
+    }
 }
