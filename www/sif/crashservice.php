a5014b3d6f26400d8f17617eb397dbeeaccb7901
jfbcable <jfbcable@372de4ae-fb28-11dd-ac5b-dfceb64ead24>
2010-01-10 21:30:35 +0000

    progress

--- www/sif/crashservice.php
+++ www/sif/crashservice.php
@@ -9,10 +9,6 @@ if (isset($_REQUEST["service"]))
 	{
 		//header("location: servicecrashswitch.php?sourcetab=".$_REQUEST["sourcetab"]."&servicetab=".$_REQUEST["servicetab"]);
 	}
-	else
-	{
-		//header('Content-type: text/plain');
-	}
 
 /*
 take the current event and divide it into 4:
@@ -36,76 +32,111 @@ take the current event and divide it into 4:
 	$stmt = $dbh->prepare($sql);
 	$stmt->execute();
 	$times = $stmt->fetch(PDO::FETCH_ASSOC);
-print_r($times);
 	if($sed=="")
 	{
-		$duration = 86400 - $times["seconds"];
-		$days = "SMTWTFS";
 		$stmt = $dbh->prepare("INSERT INTO service_active_schedule"
-								." (service,source,first_date,last_date,days,start_time,duration)"
-								." VALUES(?,?,?,?,?,?,SEC_TO_TIME(?))"
+				." (service,source,first_date,last_date,days,start_time,duration)"
+				." VALUES(?,?,?,?,?,?,SEC_TO_TIME(?))"
 			);
-		$stmt->bindParam(1, $service);
-		$stmt->bindParam(2, $new_source);
-		$stmt->bindParam(3, $times["today"]);
-		$stmt->bindParam(4, $times["today"]);
-		$stmt->bindParam(5, $days);
-		$stmt->bindParam(6, $times["start"]);
-		$stmt->bindParam(7, $duration);
+		$stmt->bindValue(1, $service);
+		$stmt->bindValue(2, $new_source);
+		$stmt->bindValue(3, $times["today"]);
+		$stmt->bindValue(4, $times["today"]);
+		$stmt->bindValue(5, "SMTWTFS");
+		$stmt->bindValue(6, $times["start"]);
+		$stmt->bindValue(7, 86400 - $times["seconds"]);
 		$stmt->execute();
 	}
 	else
 	{
 		$stmt = $dbh->prepare("SELECT s.*,"
-								." TIME_TO_SEC(duration) AS duration_seconds"
-								." TIME_TO_SEC(start_time) AS start_seconds"
-								." FROM service_active_schedule AS s WHERE service_event_id=?"
-								);
+				." TIME_TO_SEC(duration) AS duration_seconds,"
+				." TIME_TO_SEC(start_time) AS start_seconds"
+				." FROM service_active_schedule AS s WHERE service_event_id=?"
+				);
 		$stmt->bindParam(1, $sed);
 		$stmt->execute();
 		$prev = $stmt->fetch(PDO::FETCH_ASSOC);
+print_r($prev);
 
-		$dbh->beginTransaction();
-		$stmt = $dbh->prepare("UPDATE service_active_schedule SET last_date=? WHERE service_event_id=?");	
-		$stmt->bindParam(1, $times["yesterday"]);
-		$stmt->bindParam(2, $sed);
-		$stmt->execute();
-		
-		$r = $prev;
-		$stmt = $dbh->prepare("INSERT INTO service_active_schedule"
-								." (service,source,first_date,last_date,days,start_time,duration)"
-								." VALUES(?,?,?,?,?,?,SEC_TO_TIME(?))"
-			);	
-		$stmt->bindParam(1, $r->service);
-		$stmt->bindParam(2, $r->source);
-		$stmt->bindParam(3, $r->first_date);
-		$stmt->bindParam(4, $r->last_date);
-		$stmt->bindParam(5, $r->days);
-		$stmt->bindParam(6, $r->start_time);
-		$stmt->bindParam(7, $r->duration);
-print_r($r);
-		$r->first_date = $times["tomorrow"];
-		$r->duration = $prev["duration_seconds"];
-print_r($r);
-		$stmt->execute();
+		try {
+			$source = $prev["source"];
+			$first_date = $prev["first_date"];
+			$last_date = $prev["last_date"];
+			$days = $prev["days"];
+			$start_time = $prev["start_time"];
+			$duration_seconds = $prev["duration_seconds"];
 
-		$r->first_date = $times["today"];
-		$r->last_date = $times["today"];
-		$r->duration = $times["seconds"]-$prev["start_seconds"];
-		$stmt->execute();
+			$dbh->beginTransaction();
 
-		$r->source = $service;
-		$r->start_time = $times["start"];
-		$r->duration = $prev["start_seconds"]+$prev["duration_seconds"]-$times["seconds"];
-		$stmt->execute();
+			$stmt = $dbh->prepare("UPDATE service_active_schedule"
+				." SET first_date=?,"
+				." last_date=?,"
+				." duration=SEC_TO_TIME(?)"
+				." WHERE service_event_id=?"
+				);	
+			$stmt->bindValue(1, $times["today"]);
+			$stmt->bindValue(2, $times["today"]);
+			$stmt->bindValue(3, $times["seconds"]-$prev["start_seconds"]);
+			$stmt->bindValue(4, $sed);
+			$stmt->execute();
+			unset($stmt);
+			
+			$stmt = $dbh->prepare("INSERT INTO service_active_schedule"
+					." (service,source,first_date,last_date,days,start_time,duration,"
+					."start_mode,name,material_id,rot,ptt,ptt_time,owner)"
+					." VALUES(?,?,?,?,?,?,SEC_TO_TIME(?),"
+					."?,?,?,?,?,?,?)"
+				);	
+			$stmt->bindValue(1, $service);
+			$stmt->bindParam(2, $source);
+			$stmt->bindParam(3, $first_date);
+			$stmt->bindParam(4, $last_date);
+			$stmt->bindParam(5, $days);
+			$stmt->bindParam(6, $start_time);
+			$stmt->bindParam(7, $duration_seconds);
+			$stmt->bindValue(8, $prev["start_mode"]);
+			$stmt->bindValue(9, $prev["name"]);
+			$stmt->bindValue(10, $prev["material_id"]);
+			$stmt->bindValue(11, $prev["rot"]);
+			$stmt->bindValue(12, $prev["ptt"]);
+			$stmt->bindValue(13, $prev["ptt_time"]);
+			$stmt->bindValue(14, $prev["owner"]);
 
-		$dbh->commit();
+			if($prev["first_date"] != $times["today"])
+			{
+				$last_date = $times["yesterday"];
+				$stmt->execute();
+			}
+
+			if($prev["last_date"] != $times["today"])
+			{
+				$first_date = $times["tomorrow"];
+				$last_date = $prev["last_date"];
+				$stmt->execute();
+			}
+
+			$source = $new_source;
+			$first_date = $times["today"];
+			$last_date = $times["today"];
+			$start_time = $times["start"];
+			$duration_seconds = $prev["start_seconds"]+$prev["duration_seconds"]-$times["seconds"];
+			$stmt->execute();
+
+			$dbh->commit();
+
+		} catch (Exception $e) {
+
+			$dbh->rollBack();
+			echo "Failed: " . $e->getMessage();
+
+		}
 	}
 	
 	$stmt = $dbh->prepare("SELECT value FROM configuration WHERE `key`='message_bus_host'");	
 	$stmt->execute();
 	$config = $stmt->fetch(PDO::FETCH_ASSOC);
-	$sender = new Sender($config["message_bus_host"]);
+	$sender = new Sender($config["value"]);
 	$sender->send($service, "refresh");
 	$sender->send($previous_source, "refresh");
 	$sender->send($source, "refresh");
