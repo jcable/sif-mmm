be85c34d5412e230d8726c66e0c33c3170ae6360
jfbcable <jfbcable@372de4ae-fb28-11dd-ac5b-dfceb64ead24>
2010-01-20 12:48:19 +0000

    new files

---
+++ www/sif/crashswitch.js
@@ -0,0 +1,77 @@
+// toggle a button to simulate radio type buttons
+function toggleButton(elementObj, idRegex) {
+	var arraySpans = document.body.getElementsByTagName("td");
+
+	for(var i = 0; i < arraySpans.length; i++)
+	{
+		if(arraySpans[i].id.match(idRegex))
+		{
+			arraySpans[i].className = 'raised';
+		}
+	}
+	elementObj.className = 'depressed';
+}
+
+// toggles prime button and variable
+function toggleprime(elementObj)
+{
+	var takestate='unprimed';
+	if(elementObj.className='raised')
+	{
+		elementObj.className='primedepressed';
+		var arraySpans = document.body.getElementsByTagName("th");
+		takestate='raised';
+	}
+	else
+	{
+		elementObj.className='raised';
+	}
+	var arraySpans = document.body.getElementsByTagName("th");
+	for(var i = 0; i < arraySpans.length; i++)
+	{
+		if(arraySpans[i].id.match('take'))
+		{
+			arraySpans[i].className = takestate;
+		}
+	}
+}
+// submit form to do crash switch
+function crashswitch()
+{
+	if(document.getElementById("primebutton").className=='primedepressed')
+	{
+  		document.crashpanel.submit();
+  	}
+}
+
+function sourcepopup()
+{
+	sourcepopurl='sourcepopup.php?source='+document.crashpanel.source.value
+	sourcewindow=window.open (sourcepopurl, "sourcepopup", "location=0,status=0,scrollbars=1,menubar=0,width=400,height=300");
+	if (window.focus) {sourcewindow.focus()}
+}
+
+function servicepopup()
+{
+	var value = document.crashpanel.service.value;
+	if ((value != "NULL") && (value != "OFF"))
+	{
+		servicepopurl='servicepopup.php?service='+value;
+		sourcewindow=window.open (servicepopurl, "servicepopup", "location=0,status=0,scrollbars=1,menubar=0,width=400,height=300");
+		if (window.focus) {servicewindow.focus()}
+	}
+}
+
+// set source variable
+function setsource(source) {
+	document.crashpanel.source.value = source;
+}
+// set service variable
+function setservice(service) {
+	document.crashpanel.service.value = service;
+}
+
+// set listener variable
+function setlistener(listener) {
+	document.crashpanel.listener.value = listener;
+}
