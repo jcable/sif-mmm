19fc45df4473317bae96d6ae5d958cddae228f56
jfbcable <jfbcable@372de4ae-fb28-11dd-ac5b-dfceb64ead24>
2010-01-26 17:51:33 +0000

    for test

--- trunk/www/sif/crashlistener.php
+++ trunk/www/sif/crashlistener.php
@@ -31,10 +31,17 @@ if (isset($_REQUEST["service"]))
 	}
 	else
 	{
+
+		$stmt = $dbh->prepare("SELECT access, dst FROM edge_output WHERE edge=?");
+		$stmt->execute(array($listener));
+		$row = $stmt->fetch(PDO::FETCH_ASSOC);
+		$row["message"] = "oi";
+		$row["service"] = $service;
+
 		$stmt = $dbh->query("SELECT value FROM configuration WHERE `key`='message_bus_host'",  PDO::FETCH_COLUMN, 0);	
 		$config = $stmt->fetch();
 		$sender = new Sender($config);
-		$sender->send($listener, json_encode(array("message" => "oi", "service" => $service)));
+		$sender->send($listener, json_encode($row));
 		$sender->close();
 	}
 
