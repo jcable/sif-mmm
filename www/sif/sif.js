c3089a34b8faeb915acd9df9b400e6946927ef09
jfbcable <jfbcable@372de4ae-fb28-11dd-ac5b-dfceb64ead24>
2010-01-29 17:50:37 +0000

    improvements

--- www/sif/sif.js
+++ www/sif/sif.js
@@ -21,7 +21,10 @@ function selected_button(panel_id)
 	var selected = $(id).tabs('option', 'selected');
 	var bg = id+(selected+1); // selected is 0 based, tab_index is currently 1 based
 	var sb = $(bg+" .ui-state-active");
-	return sb[0].firstChild.firstChild.textContent;
+	if(sb.length>0)
+		return sb[0].firstChild.firstChild.textContent;
+	else
+		return "OFF";
 }
 
 function prime_event(event, ui)
@@ -47,6 +50,7 @@ function take_event(event, ui) {
 	var panel = myid.replace("take","");
 	var s = selected_button(panel+"_source");
 	var d = selected_button(panel+"_dest");
+	$('#'+myid.replace("take","prime")).removeClass("ui-state-active");
 	switch(panel)
 	{
 	case 'scs':
@@ -56,6 +60,7 @@ function take_event(event, ui) {
 		$.post('crashlistener.php', {'service':s,'listener':d});
 		break;
 	}
+	$(me).button('disable');
 	return false;
 }
 
@@ -66,5 +71,80 @@ function monitor_event(event, ui) {
 	var s1 = selected_button("mcs_1");
 	var s2 = selected_button("mcs_2");
 	$.post('crashmon.php', {'source':s1,'service':s2,'mon':myid});
+	return false;
+}
+
+function routingpopup(event, ui)
+{
+	var me = event.target;
+	var title = me.firstChild.textContent;
+	var dlg = $('#routing');
+	dlg.dialog('option', 'title', title);
+	var dlge = dlg.get(0);
+	$.getJSON("active.php", function(json){
+		var text = "";
+		var tabs = $("#tabs");
+		var selected = tabs.tabs('option', 'selected');
+		var tab = $("#tabs li").get(selected);
+		var panel = "";
+		if(tab.innerHTML.match(/service/i))
+		{
+			panel = "scs";
+		}
+		if(tab.innerHTML.match(/listener/i))
+		{
+			panel = "lcs";
+		}
+		if(title.match(/source/i))
+		{
+			var s = selected_button(panel+"_source");
+			var dests = "";
+			for(var i=0; i<json.length; i++)
+			{
+				if(json[i].input==s)
+					dests += "<li>"+json[i].output+"</li>";
+			}
+			if(dests = "")
+			{
+				text += "Source "+s+" is currently not routed.";
+			}
+			else
+			{
+				text += "Source "+s+" is currently routed to:<ul>";
+				text += "<ul>"+dests+"</ul>";
+			}
+		}
+		if(title.match(/service/i))
+		{
+			var s = selected_button(panel+"_dest");
+			var source = "?";
+			for(var i=0; i<json.length; i++)
+			{
+				if(json[i].output==s)
+					source = json[i].input;
+			}
+			text = "The source for "+s+" is "+source+"<p>";
+			var dests = "";
+			for(var i=0; i<json.length; i++)
+			{
+				if(json[i].input==s)
+					dests += "<li>"+json[i].output+"</li>";
+			}
+			if(dests = "")
+			{
+				text += "Service "+s+" is currently not routed.";
+			}
+			else
+			{
+				text += "Service "+s+" is currently routed to:<ul>";
+				text += "<ul>"+dests+"</ul>";
+			}
+		}
+		if(title.match(/listener/i))
+		{
+		}
+		dlge.innerHTML = text;
+		dlg.dialog('open');
+	});
 	return false;
 }
